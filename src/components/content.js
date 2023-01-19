eb473d65da0e52382de63da936571029682066b5
RiverTwilight <wrj2014@126.com>
2023-01-19 20:55:53 +0800

    style: login page

--- src/components/content.js
+++ src/components/content.js
@@ -3,6 +3,12 @@ import LoginPage from "../pages/login.jsx";
 (function main() {
 	console.log("Copy right @RiverTwilight");
 
+	disableStyle()
+
 	const currentPage = new LoginPage(window.location.pathname);
 
 })();
+
+function disableStyle(){
+	document.querySelectorAll("link[href='/css/newcss/project.css']").forEach(sheet => sheet.disabled = true)
+}
